07f593736f15c065558f76abd46ccef285bbc313
Adam Hlubina <adamhlubina@gmail.com>
2025-09-23 18:25:47 +0200

    chore: create project and workspace

---
+++ Motify/Motify/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Motify
+//
+//  Created by Adam Hlubina on 23.09.2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Motify/Motify/MotifyApp.swift
@@ -0,0 +1,17 @@
+//
+//  MotifyApp.swift
+//  Motify
+//
+//  Created by Adam Hlubina on 23.09.2025.
+//
+
+import SwiftUI
+
+@main
+struct MotifyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
